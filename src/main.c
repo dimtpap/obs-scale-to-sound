5758fa842815c8c36fe80c95d928870b76ccf3f9
Qufyy <jimpap31@outlook.com.gr>
2021-08-07 12:16:54 +0300

    Filter can now be added and removed

--- src/main.c
+++ src/main.c
@@ -2,6 +2,38 @@
 
 OBS_DECLARE_MODULE()
 
-bool obs_module_load(void) {
+const char *SOURCE_NAME = "Scale To Sound";
+
+struct scale_to_sound_data {
+	obs_source_t *context;
+};
+
+char* get_source_name(void) {
+	return SOURCE_NAME;
+}
+
+static void *create_source(obs_data_t *settings, obs_source_t *source) {
+  struct scale_to_sound_data *stsf = bzalloc(sizeof(*stsf));
+	stsf->context = source;
+	return stsf;
+}
+
+static void destroy_source(obs_data_t *data) {
+	struct scale_to_sound_data *stsf = data;
+	bfree(stsf);
+}
+
+struct obs_source_info scale_to_sound = {
+	.id           = "scale_to_sound",
+	.type         = OBS_SOURCE_TYPE_FILTER,
+	.output_flags = OBS_SOURCE_VIDEO,
+	.get_name     = get_source_name,
+  .create 			= create_source,
+	.destroy 			= destroy_source
+};
+
+bool obs_module_load(void)
+{
+	obs_register_source(&scale_to_sound);
 	return true;
 }
